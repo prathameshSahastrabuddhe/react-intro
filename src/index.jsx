b61ff7c642e7b180fb6e0fa3799c471eced63a4a
Prathamesh Sahastrabuddhe <100425816+prathameshSahastrabuddhe@users.noreply.github.com>
2025-08-09 17:12:08 +0530

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const YOURNAME = "Prathamesh Sahastrabuddhe";
+const date = new Date();
+const CURRENTYEAR = date.getFullYear();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <div>
+        <p>Created By {YOURNAME}</p>
+        <p>Copyright {CURRENTYEAR}</p>
+    </div>
+);
+
